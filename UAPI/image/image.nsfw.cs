9116417d4386977853b43e637a2fbd8cb53f6bbf
Rainbow-SPY <2716842407@qq.com>
2026-05-05 00:10:38 +0800

    add feature: 图像敏感度检查

--- UAPI/image/image.nsfw.cs
+++ UAPI/image/image.nsfw.cs
@@ -10,21 +10,34 @@ namespace UAPI
 {
     public partial class Image
     {
+        /// <summary>
+        /// 图像敏感度检查
+        /// </summary>
+        /// <param name="image">图像的二进制文件</param>
+        /// <param name="Authentication">API Token Key</param>
+        /// <returns><see cref="NSFWType"/>对象</returns>
         public static async Task<NSFWType> CheckImageNSFW(byte[] image, string Authentication = "") =>
-            await CheckImageNSFW(image, Authentication);
+            await CheckImageNSFW(image, null, Authentication);
+
+        /// <summary>
+        /// 图像敏感度检查
+        /// </summary>
+        /// <param name="url">图像的URL地址</param>
+        /// <param name="Authentication">API Token Key</param>
+        /// <returns><see cref="NSFWType"/>对象</returns>
+        public static async Task<NSFWType> CheckImageNSFW(string url, string Authentication = "") =>
+            await CheckImageNSFW(null, url, Authentication);
 
 
-        private static async Task<NSFWType> CheckImageNSFW(byte[] image,string url, string Authentication = "")
+        private static async Task<NSFWType> CheckImageNSFW(byte[] image, string url, string Authentication = "")
         {
             var contentType = "image/jpeg";
+
             if (image == null || image.Length < 8)
-            {
-                if (!string.IsNullOrEmpty(url))
-                    contentType = "application/json";
-                contentType = "application/octet-stream";
-            }
+                contentType = string.IsNullOrEmpty(url)
+                    ? "application/octet-stream"
+                    : "application/json";
             else
-            {
                 switch (image[0])
                 {
                     // 魔数判断（最准）
@@ -41,20 +54,15 @@ namespace UAPI
                         contentType = "image/webp";
                         break;
                 }
-            }
 
-            var postContent = null;
-            if (image == null)
-            {
-                if (url == null)
-                    throw new ArgumentException("image and url are null");
-                else
-                    postContent = url;
-            }
+            var postContent = image ??
+                              (object)(url ??
+                                       throw new ArgumentException("image and url are null"));
 
             var (result, statuscode) = await Interface.GetResult<NSFWType>($"{Interface._UAPI_Request_Url}image/nsfw",
-                Interface.SendRequestType.POST, image, contentType, Authentication);
-            var list = Interface.IsGetSuccessful(result, "byte[] image or string url", statuscode, new General.UAPIUnknowException(),
+                Interface.SendRequestType.POST, postContent, contentType, Authentication);
+            var list = Interface.IsGetSuccessful(result, "byte[] image or string url", statuscode,
+                new General.UAPIUnknowException(),
                 "Image.CheckImageNSFW");
             if (!list.IsRequestSuccessfully)
                 LogLibraries.WriteLog.Error($"请求失败, 请重试!\n\t返回值: {list.StatusCode}\n\t错误信息: {list.FailedReason}");
