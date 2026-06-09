0b26de2a697dea1b528b3293b295771f113224f0
Rainbow-SPY <2716842407@qq.com>
2026-06-09 19:25:41 +0800

    添加异常处理并更新示例代码
    - 在 `获取Minecraft最新版本.cs` 和 `获取bilibili投稿详情.cs` 中添加异常处理逻辑
    - 更新项目文件，增加语言版本和可空性支持
    - 更新版本号至 1.2.2

--- UAPI/Example/获取Minecraft最新版本.cs
+++ UAPI/Example/获取Minecraft最新版本.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Rox.Runtimes;
+using UAPI.IException;
 
 namespace UAPI.Example
 {
@@ -8,16 +10,35 @@ namespace UAPI.Example
     {
         public async Task Test()
         {
-            var result = await Minecraft.GetLatestVersion.FromUAPI();
-            var requestInfo = Type.Response.Headers.OutputRequestHeadersInfo(result);
-            var info = $"最新快照: {result.Snapshot}\n最新正式版: {result.Release}";
+            try
+            {
+                var result = await Minecraft.GetLatestVersion.FromUAPI();
+                var requestInfo = Type.Response.Headers.OutputRequestHeadersInfo(result);
+                var info = $"最新快照: {result.Snapshot}\n最新正式版: {result.Release}";
 
 
 // 响应体详情
-            LogLibraries.WriteLog.Info(info);
-            LogLibraries.WriteLog.Info("\n\n");
+                LogLibraries.WriteLog.Info(info);
+                LogLibraries.WriteLog.Info("\n\n");
 // 响应头详情
-            LogLibraries.WriteLog.Info(requestInfo);
+                LogLibraries.WriteLog.Info(requestInfo);
+            }
+            catch (minecraft.MojangAPIServiceError e)
+            {
+                LogLibraries.WriteLog.Error(Core.ReportText("与Mojang 通信错误", "Test", e));
+            }
+            catch (General.UAPIServerDown e)
+            {
+                LogLibraries.WriteLog.Error(Core.ReportText("UAPI服务异常", "Test", e));
+            }
+            catch (General.UAPIServiceUnavailable e)
+            {
+                LogLibraries.WriteLog.Error(Core.ReportText("指定的服务暂时不可用", "Test", e));
+            }
+            catch (Exception e)
+            {
+                LogLibraries.WriteLog.Error(Core.ReportText("未知的异常", "Test", e));
+            }
         }
     }
 }
--- UAPI/Example/获取bilibili投稿详情.cs
+++ UAPI/Example/获取bilibili投稿详情.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Rox.Runtimes;
+using UAPI.IException;
 
 namespace UAPI.Example
 {
@@ -8,35 +10,54 @@ namespace UAPI.Example
     {
         public async Task Test()
         {
-            var result = await bilibili.GetArchives(mid: "2048173282", keywords: "",
-                ps: 20, pn: 1, Authentication: "uapi-sk-********");
-            var info = $"视频总数量: {result.Total}" +
-                       $"\n\t页数: {result.PageCount}" +
-                       $"\n\t每页数量: {result.PageSize}";
-
-            var requestInfo = Type.Response.Headers.OutputRequestHeadersInfo(result);
-
-            for (var index = 0; index < result.VideosList.Count; index++)
+            try
             {
-                info = info + $"第 {index + 1} 个视频" +
-                       $"\n\t视频的标题: {result.VideosList[index].Title}" +
-                       $"\n\t视频的AVID: {result.VideosList[index].AID}" +
-                       $"\n\t视频的BVID: {result.VideosList[index].BVID}" +
-                       $"视频封面: {result.VideosList[index].CoverImageUrl}\n" +
-                       $"视频持续时长: {(result.VideosList[index].Duration < 0 ? "00:00:00" : $"{(result.VideosList[index].Duration / 3600 == 0 ? "00" : (result.VideosList[index].Duration / 3600).ToString())}: {(result.VideosList[index].Duration % 3600 / 60 == 0 ? "00" : (result.VideosList[index].Duration % 3600 / 60).ToString())}:{result.VideosList[index].Duration % 60}")}\n" +
-                       $"发布时间: {result.VideosList[index].PublishTimeStr}\n" +
-                       $"播放量: {result.VideosList[index].PlayCount}" +
-                       $"创建时间: {result.VideosList[index].CreateTimeStr}\n" +
-                       $"视频状态: {result.VideosList[index].State}\n" +
-                       $"是否为充电视频: {result.VideosList[index].IsPayVideo_str}\n" +
-                       $"是否为共创视频: {result.VideosList[index].IsInteractive}\n";
-            }
+                var result = await bilibili.GetArchives(mid: "2048173282", keywords: "",
+                    ps: 20, pn: 1, Authentication: "uapi-sk-********");
+                var info = $"视频总数量: {result.Total}" +
+                           $"\n\t页数: {result.PageCount}" +
+                           $"\n\t每页数量: {result.PageSize}";
+
+                var requestInfo = Type.Response.Headers.OutputRequestHeadersInfo(result);
+
+                for (var index = 0; index < result.VideosList.Count; index++)
+                {
+                    info = info + $"第 {index + 1} 个视频" +
+                           $"\n\t视频的标题: {result.VideosList[index].Title}" +
+                           $"\n\t视频的AVID: {result.VideosList[index].AID}" +
+                           $"\n\t视频的BVID: {result.VideosList[index].BVID}" +
+                           $"视频封面: {result.VideosList[index].CoverImageUrl}\n" +
+                           $"视频持续时长: {(result.VideosList[index].Duration < 0 ? "00:00:00" : $"{(result.VideosList[index].Duration / 3600 == 0 ? "00" : (result.VideosList[index].Duration / 3600).ToString())}: {(result.VideosList[index].Duration % 3600 / 60 == 0 ? "00" : (result.VideosList[index].Duration % 3600 / 60).ToString())}:{result.VideosList[index].Duration % 60}")}\n" +
+                           $"发布时间: {result.VideosList[index].PublishTimeStr}\n" +
+                           $"播放量: {result.VideosList[index].PlayCount}" +
+                           $"创建时间: {result.VideosList[index].CreateTimeStr}\n" +
+                           $"视频状态: {result.VideosList[index].State}\n" +
+                           $"是否为充电视频: {result.VideosList[index].IsPayVideo_str}\n" +
+                           $"是否为共创视频: {result.VideosList[index].IsInteractive}\n";
+                }
 
 // 响应体详情
-            LogLibraries.WriteLog.Info(info);
-            LogLibraries.WriteLog.Info("\n\n");
+                LogLibraries.WriteLog.Info(info);
+                LogLibraries.WriteLog.Info("\n\n");
 // 响应头详情
-            LogLibraries.WriteLog.Info(requestInfo);
+                LogLibraries.WriteLog.Info(requestInfo);
+            }
+            catch (IException.bilibili.BilibiliServiceError e)
+            {
+                LogLibraries.WriteLog.Error(Core.ReportText("bilibili API 服务异常", "Test", e));
+            }
+            catch (General.UAPIServerDown e)
+            {
+                LogLibraries.WriteLog.Error(Core.ReportText("UAPI服务异常", "Test", e));
+            }
+            catch (General.UAPIServiceUnavailable e)
+            {
+                LogLibraries.WriteLog.Error(Core.ReportText("指定的服务暂时不可用", "Test", e));
+            }
+            catch (Exception e)
+            {
+                LogLibraries.WriteLog.Error(Core.ReportText("未知的异常", "Test", e));
+            }
         }
     }
 }
